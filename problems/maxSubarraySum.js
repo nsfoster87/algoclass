a4f29ad40d7a9f206f52cb4bd21935978943ccdc
Nate Foster <nate.s.foster@gmail.com>
2023-08-08 11:12:20 -0400

    pseudocode maxSubarraySum

--- problems/maxSubarraySum.js
+++ problems/maxSubarraySum.js
@@ -4,7 +4,13 @@
 // in the array
 
 const maxSubarraySum = (arr, n) => {
-
+  // pseudocode
+  // [1,2,5,2,8,1,5], 4
+  // use a sliding window
+  // keep track of max
+  // look at i -> i+n elements until i+n > length
+  // compare to max
+  // if greater, set new max
 };
 
 module.exports = maxSubarraySum;
